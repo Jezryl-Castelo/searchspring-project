20902303b4cb563e5ed0e097362764f3cf3c31dc
Jezryl <yamabags@gmail.com>
2021-02-03 07:34:33 -0700

    add pagination and styles

--- script.js
+++ script.js
@@ -1,52 +1,113 @@
+//Initialize Variables
 const searchForm = document.querySelector('form');
 const input = document.querySelector('#search-input');
 const cardResults = document.querySelector('#cards');
-console.log(results);
+// const pageForward = document.querySelectorAll('.btn-next');
+// const pageBack = document.querySelectorAll('.btn-prev');
+//const nextPageButton = document.querySelectorAll('.nextPageButton');
+//const prevPageButton = document.querySelectorAll('.prevPageButton');
+const pageButtons = document.querySelectorAll('.pageButtons');
+const pageSpan = document.getElementById('page');
+let pageState = {};
+let currentPage = 1;
+
+//Setup Event Listeners
 searchForm.addEventListener('submit', search);
-//what happens on submit
+
+pageButtons.forEach((button) =>
+  button.addEventListener('click', handlePageButtonClick),
+);
+
+//JS Methods
 
 function search(e) {
-    e.preventDefault();
-    searchInput = searchForm.elements.search.value;
-    console.log(searchInput)
-    
-    fetchAPI();
-};
-
-async function fetchAPI() {
-    const searchURL = `http://api.searchspring.net/api/search/search.json?siteId=scmq7n&q=${searchInput}&resultsFormat=native&page`;
-    const response = await fetch(searchURL);
-    const data = await response.json();
-    generateHTML(data.results);
-    console.log(data);
+  e.preventDefault();
+  currentPage = 1;
+  fetchAPI(currentPage);
 }
 
-function generateHTML(results) {
-    let generatedHTML = '';
-    results.map((result) => {
-        generatedHTML +=
-        `<div class="cards">
-           <img  image" src="${result.thumbnailImageUrl}" alt="result">
+async function fetchAPI(currentPage) {
+  let searchInput = document.getElementById('search-input').value;
+  const searchURL = `http://api.searchspring.net/api/search/search.json?siteId=scmq7n&q=${searchInput}&resultsFormat=native&page=${currentPage}`;
+  const data = await fetch(searchURL)
+    .then((response) => response.json())
+    .catch((err) => console.log(`Error ${err}`));
+
+  generateHTML(data.results);
+  pageState = data.pagination;
+  pageSpan.innerHTML = currentPage;
+}
+
+function generateHTML(searchResults) {
+  // console.log(searchResults);
+  let generatedHTML = '';
+
+  searchResults.map((singleResult) => {
+    const { thumbnailImageUrl, title, msrp, price } = singleResult;
+    let msrpPrice = +msrp;
+    let salePrice = +price;
+    // if (singleResult.title.length > 10) {
+    //   singleResult.title = `${singleResult.title.substr(0, 10)}...`;
+    // }
+
+    generatedHTML += `<div class="card">
+    <button class=></button>
+           <img  class="card-image" src="${thumbnailImageUrl}" alt="result image">
            <div class="details">
-           <p class="product">${result.title}</p>
-           <p class="sale">$${result.price}.00</p>
-           <p class="msrp">$${result.msrp}.00</p>
+          <p class="product">${title}</p>
+          ${
+            msrp
+              ? `<p class=${
+                  msrpPrice > salePrice ? 'msrp-strike' : 'mspr'
+                }>$${msrpPrice.toFixed(2)}</p>`
+              : ''
+          }
+           <p class="sale">$${salePrice.toFixed(2)}</p>
            </div>
-        </div>`
-    })
-    cardResults.innerHTML = generatedHTML;
+        </div>`;
+  });
+
+  cardResults.innerHTML = generatedHTML;
 }
 
-
-
-
-//---dropdown for filters ---
-const showMoreBtn = document.querySelectorAll('.dropdown-btn');
-const dropdownDetails = document.querySelectorAll('dropdown-details');
-
-for(let i = 0; i < showMoreBtn.length; i++) {
-    showMoreBtn[i].addEventListener('click', function() {
-
-    })
+//Page Click Functionality
+function buttonDisable(e) {
+  if (currentPage === 1) {
+    console.log(e.target);
+  } else if (currentPage === pageState.totalPages) {
+  }
 }
-// const searchURL = 'http://api.searchspring.net/api/search/search.json?siteId=scmq7n&q=jeans&resultsFormat=native&page=2';
+
+function handlePageButtonClick(e) {
+  if (e.target.innerHTML === 'Next') {
+    pageClickForward();
+  } else {
+    pageClickBack();
+  }
+}
+
+function pageClickForward() {
+  if (currentPage < pageState.totalPages) {
+    currentPage++;
+    fetchAPI(currentPage);
+  }
+}
+
+function pageClickBack() {
+  if (currentPage !== 1) {
+    currentPage--;
+    fetchAPI(currentPage);
+  }
+}
+
+// use classList to change text
+// function onSale() {
+//   const msrpText = document.querySelectorAll('.msrp');
+//   const saleText = document.querySelectorAll('.sale');
+//   msrpText.forEach((item) => {
+//     console.log(item);
+//   });
+// if (saleText < msrpText) {
+//   msrpText.classList.add('strike');
+// }
+// }
